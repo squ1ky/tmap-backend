d93cc24b592c11fd527c2eedf927894c61b92ce2
Alexei <parshevalexsei@gmail.com>
2026-04-20 21:36:38 +0300

    feat: map districtimageurl in cluster popup response

--- heatmap-api/src/main/java/ru/tbank/tmap/service/H3HeatmapService.java
+++ heatmap-api/src/main/java/ru/tbank/tmap/service/H3HeatmapService.java
@@ -66,6 +66,7 @@ public class H3HeatmapService implements HeatmapService {
                 .map(cluster -> new ClusterDetailsResponse()
                         .h3Index(Long.toHexString(cluster.h3Index()))
                         .resolution(cluster.resolution().getValue())
+                        .districtImageUrl("")
                         .txCount(cluster.txCount())
                         .avgCheck(cluster.avgCheck().doubleValue())
                         .sumAmount(cluster.sumAmount().doubleValue())

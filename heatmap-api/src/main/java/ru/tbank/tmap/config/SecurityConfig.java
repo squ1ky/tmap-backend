9ac2623cb45a1962cca7dbf8fb75c53661aeb747
squ1ky <squ1ky@mail.ru>
2026-04-22 08:58:04 +0300

    chore: set up SecurityConfig to work with JWT and add requestMatchers for different paths

--- heatmap-api/src/main/java/ru/tbank/tmap/config/SecurityConfig.java
+++ heatmap-api/src/main/java/ru/tbank/tmap/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package ru.tbank.tmap.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,13 +19,20 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/heatmap/clusters", "/api/v1/heatmap/clusters/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
+                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "api/v1/auth/refresh").permitAll()
+                        .requestMatchers("/api/v1/heatmap/**").permitAll()
+                        .requestMatchers("/api/v1/venues", "/api/v1/venues/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

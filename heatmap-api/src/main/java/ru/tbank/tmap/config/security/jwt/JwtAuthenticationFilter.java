a8a26edef6167c565b911a666afa170099d99c59
squ1ky <squ1ky@mail.ru>
2026-04-22 18:08:27 +0300

    test: add tests for service/auth and AuthControllerTest

--- heatmap-api/src/main/java/ru/tbank/tmap/config/security/jwt/JwtAuthenticationFilter.java
+++ heatmap-api/src/main/java/ru/tbank/tmap/config/security/jwt/JwtAuthenticationFilter.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import ru.tbank.tmap.service.auth.JpaUserDetailsService;
 import ru.tbank.tmap.service.auth.jwt.JwtService;
 
 import java.io.IOException;
@@ -25,7 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String BEARER_PREFIX = "Bearer ";
 
     private final JwtService jwtService;
-    private final JpaUserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(

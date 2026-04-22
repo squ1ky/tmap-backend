490928cf4fe950e1be86922e16a42edc46889be6
squ1ky <squ1ky@mail.ru>
2026-04-22 18:21:49 +0300

    chore: add actuator from main branch

--- heatmap-api/src/main/java/ru/tbank/tmap/config/security/SecurityConfig.java
+++ heatmap-api/src/main/java/ru/tbank/tmap/config/security/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
             "/error",
             "/swagger-ui/**",
             "/api-docs/**",
+            "/actuator/health",
+            "/actuator/health/**"
     };
 
     private static final String ADMIN_ENDPOINTS = "/api/v1/admin/**";

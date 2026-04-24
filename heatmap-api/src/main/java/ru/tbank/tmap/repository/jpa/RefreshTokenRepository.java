04c85a21fc02661e7d6e4e3701fa63510458a173
squ1ky <squ1ky@mail.ru>
2026-04-24 19:15:51 +0300

    bugfix: add pessimistic lock to method findByTokenHash() in RefreshTokenRepository

--- heatmap-api/src/main/java/ru/tbank/tmap/repository/jpa/RefreshTokenRepository.java
+++ heatmap-api/src/main/java/ru/tbank/tmap/repository/jpa/RefreshTokenRepository.java
@@ -1,6 +1,10 @@
 package ru.tbank.tmap.repository.jpa;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import ru.tbank.tmap.domain.user.RefreshToken;
 
 import java.util.Optional;
@@ -8,5 +12,7 @@ import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
     Optional<RefreshToken> findByTokenHash(String tokenHash);
 }
